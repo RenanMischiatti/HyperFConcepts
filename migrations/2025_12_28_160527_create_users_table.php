514edfd63d82cfa542283826041ce73be611b6d6
RenanMischiatti <renan.mischiatti@hotmailcom>
2025-12-28 16:19:09 -0300

    [ FEAT ] Create user

--- migrations/2025_12_28_160527_create_users_table.php
+++ migrations/2025_12_28_160527_create_users_table.php
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->datetimes();
+            $table->string('name', 100);
+            $table->string('email', 150)->unique();
+            $table->string('password');
+            $table->timestamps();
         });
     }
 
